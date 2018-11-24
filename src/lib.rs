59a5d01db16e4ba403f0a17032106e8a34af411e
George Kaplan <george@georgekaplan.xyz>
2018-11-24 17:09:49 -0500

    added labels and param setter/getter

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,17 @@
 use vst2::buffer::AudioBuffer;
 use vst2::plugin::{Plugin, Info};
 
-#[derive(Default)]
-struct QDist;
+struct QDist {
+    threshold: f32
+}
+
+impl Default for QDist {
+    fn default() -> QDist {
+        QDist {
+            threshold: 1.0
+        }
+    }
+}
 
 impl Plugin for QDist {
     fn get_info(&self) -> Info {
@@ -18,6 +27,41 @@ impl Plugin for QDist {
             parameters: 1,
 
             ..Info::default()
+        }
+    }
+
+    fn get_parameter(&self, index: i32) -> f32 {
+        match index {
+            0 => self.threshold,
+            _ => 0.0,
+        }
+    }
+
+    fn set_parameter(&mut self, index: i32, value: f32) {
+        match index {
+            0 => self.threshold = value.max(0.01),
+            _ => (),
+        }
+    }
+
+    fn get_parameter_name(&self, index: i32) -> String {
+        match index {
+            0 => "Threshold".to_string(),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter_text(&self, index: i32) -> String {
+        match index {
+            0 => format!("{}", self.threshold * 100.0),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter_label(&self, index: i32) -> String {
+        match index {
+            0 => "%".to_string(),
+            _ => "".to_string(),
         }
     }
 }
