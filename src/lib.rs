b9c2a12869ebea16453036fcfbac7a1137123753
George Kaplan <george@georgekaplan.xyz>
2018-11-24 21:42:17 -0500

    added bitcrush

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use vst::plugin::{Plugin, Info};
 struct QDist {
     threshold: f32,
     bias: f32,
+    bits: u8,
 }
 
 impl Default for QDist {
@@ -15,6 +16,7 @@ impl Default for QDist {
         QDist {
             threshold: 1.0,
             bias: 0.0,
+            bits: 32,
         }
     }
 }
@@ -28,7 +30,7 @@ impl Plugin for QDist {
 
             inputs: 2,
             outputs: 2,
-            parameters: 2,
+            parameters: 3,
 
             ..Info::default()
         }
@@ -38,6 +40,7 @@ impl Plugin for QDist {
         match index {
             0 => self.threshold,
             1 => self.bias / 2.0 + 0.5,
+            2 => f32::from(self.bits) / 32.0,
             _ => 0.0,
         }
     }
@@ -46,6 +49,7 @@ impl Plugin for QDist {
         match index {
             0 => self.threshold = value.max(0.01),
             1 => self.bias = value * 2.0 - 1.0,
+            2 => self.bits = (value * 32.0).max(1.0).round() as u8,
             _ => (),
         }
     }
@@ -54,6 +58,7 @@ impl Plugin for QDist {
         match index {
             0 => "Threshold".to_string(),
             1 => "DC Offset".to_string(),
+            2 => "Bit Depth".to_string(),
             _ => "".to_string(),
         }
     }
@@ -62,6 +67,7 @@ impl Plugin for QDist {
         match index {
             0 => format!("{}", self.threshold * 100.0),
             1 => format!("{}", self.bias),
+            2 => format!("{}", self.bits),
             _ => "".to_string(),
         }
     }
@@ -70,17 +76,16 @@ impl Plugin for QDist {
         match index {
             0 => "%".to_string(),
             1 => "".to_string(),
+            2 => "bits".to_string(),
             _ => "".to_string(),
         }
     }
 
     fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
         for (input, output) in buffer.zip() {
-            for (input_sample, output_sample) in
-                input.into_iter().zip(output.into_iter())
-            {
-
-                *output_sample = thresh(*input_sample + self.bias, self.threshold);
+            for (input_sample, output_sample) in input.into_iter().zip(output.into_iter()) {
+                *output_sample =
+                    bit32(thresh(*input_sample + self.bias, self.threshold), self.bits);
             }
         }
     }
@@ -95,6 +100,11 @@ where
     } else {
         input.min(threshold) / threshold
     }
+}
+
+fn bit32(input: f32, depth: u8) -> f32 {
+    let shift = 32 - depth;
+    f32::from_bits((input.to_bits() >> shift) << shift)
 }
 
 plugin_main!(QDist);
