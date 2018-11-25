e5ae793cd1796bff60384894f00f8b648e3c5a00
George Kaplan <george@georgekaplan.xyz>
2018-11-24 20:42:33 -0500

    moved thresholding into its own fn, added bias parameter

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 #[macro_use] extern crate vst;
+extern crate num_traits;
 
 use vst::buffer::AudioBuffer;
 use vst::plugin::{Plugin, Info};
 
 struct QDist {
-    threshold: f32
+    threshold: f32,
+    bias: f32,
 }
 
 impl Default for QDist {
     fn default() -> QDist {
         QDist {
-            threshold: 1.0
+            threshold: 1.0,
+            bias: 0.0,
         }
     }
 }
@@ -24,7 +27,7 @@ impl Plugin for QDist {
 
             inputs: 2,
             outputs: 2,
-            parameters: 1,
+            parameters: 2,
 
             ..Info::default()
         }
@@ -33,6 +36,7 @@ impl Plugin for QDist {
     fn get_parameter(&self, index: i32) -> f32 {
         match index {
             0 => self.threshold,
+            1 => self.bias,
             _ => 0.0,
         }
     }
@@ -40,6 +44,7 @@ impl Plugin for QDist {
     fn set_parameter(&mut self, index: i32, value: f32) {
         match index {
             0 => self.threshold = value.max(0.01),
+            1 => self.bias = value * 2.0 - 1.0,
             _ => (),
         }
     }
@@ -47,6 +52,7 @@ impl Plugin for QDist {
     fn get_parameter_name(&self, index: i32) -> String {
         match index {
             0 => "Threshold".to_string(),
+            1 => "DC Offset".to_string(),
             _ => "".to_string(),
         }
     }
@@ -54,6 +60,7 @@ impl Plugin for QDist {
     fn get_parameter_text(&self, index: i32) -> String {
         match index {
             0 => format!("{}", self.threshold * 100.0),
+            1 => format!("{}", self.bias),
             _ => "".to_string(),
         }
     }
@@ -61,6 +68,7 @@ impl Plugin for QDist {
     fn get_parameter_label(&self, index: i32) -> String {
         match index {
             0 => "%".to_string(),
+            1 => "".to_string(),
             _ => "".to_string(),
         }
     }
@@ -69,14 +77,18 @@ impl Plugin for QDist {
         for (input_buffer, output_buffer) in buffer.zip() {
             for (input_sample, output_sample) in input_buffer.into_iter().zip(output_buffer.into_iter()) {
 
-                if *input_sample >= 0.0 {
-                    *output_sample = input_sample.min(self.threshold) / self.threshold
-                }
-                else {
-                    *output_sample = input_sample.min(-self.threshold) / self.threshold
-                }
+                *output_sample = thresh(*input_sample + self.bias, self.threshold);
             }
         }
+    }
+}
+
+fn thresh<T>(input: T, threshold: T) -> T
+where T: num_traits::float::Float + Default {
+    if input < T::default() {
+        input.min(-threshold) / threshold
+    } else {
+        input.min(threshold) / threshold
     }
 }
 
